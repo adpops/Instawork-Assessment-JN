5b8f6dff970811235e97611f79159c7d07b7f9ba
JN <jaimin2468@gmail.com>
2022-07-19 15:00:51 -0700

    added add and edit url in url.py

--- manageApp/views.py
+++ manageApp/views.py
@@ -7,8 +7,10 @@ def index(request):
     context = {'memberLst':memberLst, 'memNum':memNum}
     return render(request, 'manageApp/index.html', context)
 
-def add(request, memberId):
+def add(request, pk):
     return render(request, 'manageApp/add.html',)
 
-def edit(request, memberId):
-    return render(request, 'manageApp/edit.html')
+def edit(request, pk):
+    member = get_object_or_404(TeamMember, pk=pk)
+    context = {'member':member}
+    return render(request, 'manageApp/edit.html', context)
