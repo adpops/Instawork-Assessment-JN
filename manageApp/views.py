e15382b982ca0880ab6a0723d41307af3403f699
JN <jaimin2468@gmail.com>
2022-07-28 13:47:53 -0700

    cleaning up code

--- manageApp/views.py
+++ manageApp/views.py
@@ -5,8 +5,6 @@ from django.shortcuts import get_object_or_404, render
 from .models import TeamMember
 from .forms import MemberForm
 import phonenumbers
-import re
-
 
 def checkValid(form):
     errorMsg = ""
@@ -25,15 +23,8 @@ class IndexView(ListView):
         context['memNum'] = context['memberLst'].count()
         return context
 
-# def index(request):
-#     memberLst = TeamMember.objects.all()
-#     memNum = memberLst.count()
-#     context = {'memberLst':memberLst, 'memNum':memNum}
-#     return render(request, 'manageApp/index.html', context)
-
 class AddView(CreateView):
     model = TeamMember
-    # fields = ['firstName', 'lastName', 'email', 'phoneNum', 'role']
     form_class = MemberForm
     template_name = 'manageApp/add.html'
     success_url = "/"
@@ -44,28 +35,6 @@ class AddView(CreateView):
             self.object = form.save()        
             return super().form_valid(form)
         return render(self.request, 'manageApp/add.html', {'form': form, 'errorMsg': errorMsg})        
-
-# def add(request, isEdit):
-#     if(request.method == 'POST'):
-#         form = MemberForm(request.POST)
-#         if(form.is_valid()):
-            
-#             memberLst = TeamMember.objects.all()
-#             for mem in memberLst:
-#                 if(mem.checkDuplicate(form)):
-#                     if(isEdit):
-#                         return HttpResponseRedirect('/') 
-#                     error_msg = "Team Member already exists"
-#                     return render(request, 'manageApp/add.html', {'form': form, 'error_msg': error_msg})
-        
-#             member = TeamMember(firstName=form.cleaned_data['firstName'], lastName=form.cleaned_data['lastName'], 
-#             email=form.cleaned_data['email'], phoneNum=form.cleaned_data['phoneNum'], role=form.cleaned_data['role'])
-#             member.save()
-#             return HttpResponseRedirect('/')
-#     else:
-#         form = MemberForm()
-#     error_msg = ""
-#     return render(request, 'manageApp/add.html', {'form': form, 'error_msg': error_msg})
 
 class EditView(UpdateView):
     model = TeamMember
@@ -84,18 +53,6 @@ class EditView(UpdateView):
         context = super().get_context_data(**kwargs)    
         context['id'] = self.kwargs['pk']
         return context
-    
-# def edit(request, pk):
-#     member = get_object_or_404(TeamMember, pk=pk)
-#     data = {
-#         'firstName': member.firstName,
-#         'lastName': member.lastName,
-#         'email': member.email,
-#         'phoneNum': member.phoneNum,
-#         'role': member.role
-#         }
-#     form = MemberForm(data)    
-#     return render(request, 'manageApp/edit.html', {'form':form})
 
 def delete(request, pk):
     member = get_object_or_404(TeamMember, pk=pk)
